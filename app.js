011ced6cdd290f4a1a8ff19bb6918fc9d971d438
GuillenME <guillenmariana550@gmail.com>
2025-12-04 12:14:20 -0600

    App mejorada para las pruebas 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hola desde Docker con GitHub! Practicando CI/CD"));
+app.get("/", (req, res) => res.send("Hola desde Docker con GitHub! Practicando CI/CD, segunda prueba."));
 
 app.listen(3000, () => console.log("App running on port 3000"));
